b3855fdcf8ec7d1ba285f1b3b0ddc97e4870a699
snt-nbzw <sntnbzw@gmail.com>
2020-03-09 01:09:49 +0900

    AppConstを追加

---
+++ HiraganaTranslationApp/Commons/AppConst.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct AppConst {
+    // swiftlint:disable identifier_name
+    static let ok = "OK"
+    static let error = "error"
+    static let hiragana = "hiragana"
+    static let translationViewController = "TranslationViewController"
+}
