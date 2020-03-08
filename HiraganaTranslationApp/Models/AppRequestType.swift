b7bf7e38e530ac9a46ca0f5b568d55493a86f097
snt-nbzw <sntnbzw@gmail.com>
2020-03-09 01:09:05 +0900

    AppRequestTypeを追加

---
+++ HiraganaTranslationApp/Models/AppRequestType.swift
@@ -0,0 +1,40 @@
+import APIKit
+
+public class DecodableDataParser: DataParser {
+    public var contentType: String? {
+        return "application/json"
+    }
+
+    public func parse(data: Data) throws -> Any {
+        return data
+    }
+}
+
+public protocol AppRequestType: Request {}
+
+extension AppRequestType {
+
+    public var baseURL: URL {
+        return URL(string: "https://labs.goo.ne.jp/api")!
+    }
+
+    public var headerFields: [String: String] {
+        return  ["Content-Type": "application/json",
+                 "charset": "utf-8"]
+    }
+}
+
+extension AppRequestType where Response: Decodable {
+    public var dataParser: DataParser {
+        return DecodableDataParser()
+    }
+
+    public func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
+        guard let data = object as? Data else {
+            throw ResponseError.unexpectedObject(object)
+        }
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try jsonDecoder.decode(Response.self, from: data)
+    }
+}
