1d82583b4ef4c200a678a8eb43da18a3974bd429
snt-nbzw <sntnbzw@gmail.com>
2020-03-08 16:07:12 +0900

    TranslationViewController .swift / nibを追加

--- HiraganaTranslationApp/AppDelegate.swift
+++ HiraganaTranslationApp/AppDelegate.swift
@@ -6,7 +6,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = TranslationViewController()
+        window?.makeKeyAndVisible()
         return true
-
     }
 }
---
+++ HiraganaTranslationApp/TranslationViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class TranslationViewController: UIViewController {
+
+    init() {
+        super.init(nibName: "TranslationViewController", bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
