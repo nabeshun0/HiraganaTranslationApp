33c1d666b9d7a98dce47dfb0348f1c8859a636b2
snt-nbzw <sntnbzw@gmail.com>
2020-03-11 01:12:50 +0900

    テストコード作成 / HiraganaTranslatorDataStoreTests() close #16

--- HiraganaTranslationAppTests/HiraganaTranslatorDataStoreTests.swift
+++ HiraganaTranslationAppTests/HiraganaTranslatorDataStoreTests.swift
@@ -1,3 +1,5 @@
+import APIKit
+@testable import HiraganaTranslationApp
 import XCTest
 
 class HiraganaTranslatorDataStoreTests: XCTestCase {
@@ -10,4 +12,19 @@ class HiraganaTranslatorDataStoreTests: XCTestCase {
         super.tearDown()
     }
 
+    func testHiraganaTranslatorDataStore() {
+        let exp = expectation(description: "HiraganaTranslator")
+
+        Session.send(HiraganaTranslatorAPI.Request(sentence: "入力してください"), callbackQueue: nil) { (result) in
+            switch result {
+            case .success(let response):
+                print(response)
+                exp.fulfill()
+            case .failure(let error):
+                exp.fulfill()
+                XCTAssert(false, error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 3.0, handler: nil)
+    }
 }
