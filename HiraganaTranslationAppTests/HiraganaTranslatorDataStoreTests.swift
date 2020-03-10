81e9663aa10a6192273f9258ae39f04b804a5d22
snt-nbzw <sntnbzw@gmail.com>
2020-03-11 00:33:30 +0900

    UnitTestBundleを追加

---
+++ HiraganaTranslationAppTests/HiraganaTranslatorDataStoreTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+
+class HiraganaTranslatorDataStoreTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
